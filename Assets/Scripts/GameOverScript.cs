8991b414fabbd82a757621b8c100bc7f04a70ec0
Ernesto Janebro <ernesto.camurca@gmail.com>
2015-06-24 22:07:07 -0300

    SFX stuff

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -6,11 +6,13 @@ public class GameOverScript : MonoBehaviour {
 
 	public Text scoreText;
 	public Text bestScoreText;
+    public AudioClip SFX;
 
 	void Start () 
 	{
         this.scoreText.text = PlayerPrefs.GetInt("Score") +"";
         this.bestScoreText.text = PlayerPrefs.GetInt("BestScore") +"";
+        gameObject.GetComponent<AudioSource>().PlayOneShot(SFX);
 	}
 
     public void LoadLevel() 
--- Assets/Scripts/StartGameScript.cs
+++ Assets/Scripts/StartGameScript.cs
@@ -4,9 +4,12 @@ using System;
 
 public class StartGameScript : MonoBehaviour {
 
+    public AudioClip SFX;
+
     public void LoadGame() 
     {
         PlayerPrefs.SetInt("BestScore", 0);
+        gameObject.GetComponent<AudioSource>().PlayOneShot(SFX);
         Application.LoadLevel(1);
     }
 }
